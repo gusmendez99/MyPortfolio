3197a33376e6edaae2e5517cac37b194e721ff88
Gus Mendez <gus.mendez.99@gmail.com>
2019-06-04 00:04:20 -0600

    Updates

---
+++ precache-manifest.b8590e4192df6f057af696c4f94443d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "039fb50aed80df3e987b",
+    "url": "/static/js/main.039fb50a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "039fb50aed80df3e987b",
+    "url": "/static/css/main.c3be39ae.chunk.css"
+  },
+  {
+    "revision": "a5f9f70f22c53a136c73727726e2cc05",
+    "url": "/index.html"
+  }
+];
